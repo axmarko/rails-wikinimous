77c74750d0d68665f4a8c7b722d4ca8079d0b2c4
Axel Marko <axkmar@gmail.com>
2019-06-17 19:28:52 +0300

    Edit/udpate/destroy still to do

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,16 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    Article.create(articles_params)
+    redirect_to articles_path
   end
 
   def edit
@@ -19,5 +23,11 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :content)
   end
 end
